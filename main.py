e025ba842746ea7cdb6525e16a9201ac6dbd757d
Hinski <poldi@mail.com>
2025-02-15 21:20:05 +0100

    created a game loop and a constants file

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+pygame.init()
+from constants import *
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+#
+#print(f"Screen width: {SCREEN_WIDTH}")
+#print(f"Screen height: {SCREEN_HEIGHT}")
+#
+
+
+def main():
+    print("Starting asteroids!")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill(color="black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
+
