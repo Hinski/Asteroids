1e4887d4812cea94bcc42bfb7e57d870ca1c04dd
Hinski <poldi@mail.com>
2025-02-16 13:33:03 +0100

    created asteroid and asteroid field classes

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import pygame
-pygame.init()
 from constants import *
+from player import Player
 
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-#
-#print(f"Screen width: {SCREEN_WIDTH}")
-#print(f"Screen height: {SCREEN_HEIGHT}")
-#
 
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatable, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+   
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color="black")
+#        player.draw(screen)
+#        player.update(dt)
+
+        updatable.update(dt)
+        
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+    
 
 if __name__ == "__main__":
     main()
